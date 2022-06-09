633dc4dd512a260c0f16d66382ab4152bf214c2e
Jonah <espinosajonah1245@gmail.com>
2022-06-09 16:45:55 -0400

    commit tree 9663b45700a0adfcf45764821a4376807e8bf0ec

---
+++ index.js
@@ -0,0 +1,8 @@
+function divide(number1,number2){
+    const num1 = 2000
+    const num2 = 100
+    return num1 / num2;
+}
+const square = (parameter1) => parameter1 * parameter1
+
+const add = (parameter1,parameter2) => parameter1 + parameter2
